7ed0e442da16cade87045cb8a28dc9700a6d8cac
kirti1parate <138138565+kirti1parate@users.noreply.github.com>
2023-10-27 16:32:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -61,13 +61,3 @@ tags= {
   }
 }
 
-#Create dynamodb table
-resource "aws_dynamodb_table" "terraform_state_lock" {
-  name           = "terraform-state-lock"
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "LockID"
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
