05778f24cd09478acc248887f30b482a6d3db9bb
kirti1parate <138138565+kirti1parate@users.noreply.github.com>
2023-10-27 16:32:38 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -6,3 +6,15 @@ terraform {
     dynamodb_table = "terraform-state-lock"
   }
 }
+
+#Create dynamodb table
+resource "aws_dynamodb_table" "terraform_state_lock" {
+  name           = "terraform-state-lock"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "Lock(M)"
+  attribute {
+    name = "Lock(M)"
+    type = "S"
+  }
+}
+
