a6e20fccee24b8b7038373f085715609ad0b4585
kirti1parate <138138565+kirti1parate@users.noreply.github.com>
2023-10-28 10:23:58 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,16 +1,19 @@
 resource "aws_s3_bucket" "my-s3-bucket" {
   bucket_prefix = var.bucket_prefix
-  acl = var.acl
-  
-   versioning {
-    enabled = var.versioning
-  }
   
   tags = var.tags
 }
 
+resource "aws_s3_bucket_acl" "example" {
+  bucket = aws_s3_bucket.my-s3-bucket.id
+  acl    = var.acl
+}
+
+
+
 resource "aws_s3_bucket_versioning" "my-s3-bucket-versioning" {
   bucket = aws_s3_bucket.my-s3-bucket.id
+  
  versioning_configuration {
     status = "Enabled"
   }
