faf74c7695b5cddd3ff4d2d8766b49baf105345c
kirti1parate <138138565+kirti1parate@users.noreply.github.com>
2023-10-28 11:10:07 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "key_name" { 
     description = " SSH keys to connect to ec2 instance" 
-    default     =  "Kirtikeypair.pem" 
+    default     =  "/var/lib/jenkins/workspace/Kirtikeypair.pem" 
 }
 
 variable "instance_type" { 
